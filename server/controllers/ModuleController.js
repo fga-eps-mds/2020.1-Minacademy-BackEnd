129714090eab0d286a765e73ce7c520467827aed
estevaoreis <estevaoreis1587@gmail.com>
2020-09-18 21:20:16 -0300

    Remoção da Controller Module

--- server/controllers/ModuleController.js
+++
@@ -1,9 +0,0 @@
-const Module = require('../models/Module');
-
-module.exports = {
-    async getUsers(req, res, next){
-        const modules = await Module.find();
-
-        return res.json(modules);
-    },
-};
