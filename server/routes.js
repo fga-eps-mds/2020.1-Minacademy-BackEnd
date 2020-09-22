6a3ceec85732b5860b33dac6d5f821ccfc4b62aa
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-22 11:03:00 -0300

    Routes: Adicionado middleware no endpoint Logout

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const UserController = require('./controllers/UserController')
+const UserController = require('./controllers/UserController');
+const auth = require('./middleware/userAuth');
 
 router.get('/users', UserController.getUsers);
 router.post('/users', UserController.createUser);
 router.delete('/users', UserController.removeUser);
 router.post('/users/login', UserController.authLogin);
-router.post('/users/logout', UserController.authLogout);
+router.post('/users/logout', auth, UserController.authLogout);
 
 module.exports = router;
