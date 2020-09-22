1846416de9ca1a42d2e1f7e702c2ac533e1fe30e
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-22 10:58:17 -0300

    UserController: Configurado para usar cookies
    - Endpoint de login e registro envia cookie com a token de acesso
    - Endpoint logout remove token de acesso
    - Melhorias de qualidade de codigo

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,61 +1,61 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { restart } = require('nodemon');
 const User = require('../models/User');
 const userAuth = require('../config/userAuth')
-const { findOne } = require('../models/User');
 
 module.exports = {
-    async getUsers(req, res, next) {
+    async getUsers(req, res) {
         const users = await User.find();
         return res.json(users);
     },
 
-    async createUser(req, res, next) {
+    async createUser(req, res) {
         try {
-            const  email = req.body.email;
-            if (await User.findOne({ email }))
-                return res.status(400).send({ error: 'Email already used' });
-
             const user = await User.create(req.body);
-            user.password = undefined;
-            
-            const accessToken = jwt.sign({id: email}, userAuth.secret, {
-                expiresIn: 300
-            });
-            return res.status(201).send({user, accessToken});
+            const accessToken = jwt.sign({ id: user.email }, userAuth.secret);
+            user.tokens = user.tokens.concat({ accessToken })
+            await user.save()
+            res.cookie('auth_token', accessToken)
+            return res.status(201).send({ user, accessToken });
         } catch (err) {
             return res.status(400).send({ error: err.message });
         }
     },
 
-    async authLogin(req, res, next) {
-        const {email, password} = req.body;
-        const user = await User.findOne({email}).select('+password');
-
-        if (user == null)
-            return res.status(400).send({ error: 'Invalid Email or Password' });
-
-        if (!bcrypt.compareSync(password, user.password))
-            return res.status(400).send({ error: 'Invalid Email or Password' });
-
-        user.password = undefined;
-
+    async authLogin(req, res) {
+        const { email, password } = req.body
         try {
-            const accessToken = jwt.sign({id: email}, userAuth.secret, {
-                expiresIn: 300
-            });
-            return res.status(201).send({ user, accessToken });
+            const user = await User.findOne({ email })
+            if (!user) {
+                throw new Error('Invalid Email or Password')
+            }
+            const isMatch = await bcrypt.compare(password, user.password)
+            if (!isMatch) {
+                throw new Error('Invalid Email or Password')
+            }
+
+            const accessToken = jwt.sign({ id: user.email }, userAuth.secret);
+            user.tokens = user.tokens.concat({ accessToken })
+            await user.save()
+            res.cookie('auth_token', accessToken, {httpOnly: false})
+            res.send({ user, accessToken })
         } catch (err) {
-            return res.status(400).send({ error: 'Login Failed' });
+            return res.status(400).send({ error: err.message });
         }
     },
 
-    async authLogout(req, res, next) {
-        res.send({ user, accessToken: null });
+    async authLogout(req, res) {
+        try {
+            req.user.tokens = req.user.tokens.filter(token => { return token.accessToken !== req.token })
+            await req.user.save()
+
+            res.send({ logout: 'Logged out'})
+         } catch (error) {
+            res.status(500).send(error)
+         }
     },
-    
-    async removeUser(req, res, next) {
+
+    async removeUser(req, res) {
         try {
             const { _id } = req.body;
             const user = await User.findOneAndDelete({ _id });
