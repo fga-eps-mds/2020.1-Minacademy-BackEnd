0d05a32e81731e8e17d50788ee147d0ec64675d9
Lucas Melo <lucasmelodos322@gmail.com>
2020-09-23 18:13:53 -0300

    mudando a funcao de editar e o endpoint

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const userAuth = require('../config/userAuth')
+const userAuth = require('../config/userAuth');
+const { find } = require('../models/User');
 
 module.exports = {
     async getUsers(req, res) {
@@ -67,8 +68,12 @@ module.exports = {
     },
 
     async editUser(req, res) {
+        const token = req.cookies['auth_token']
+        const decodedToken = jwt.verify(token, userAuth.secret)
+        const email = decodedToken.id;
+        const user = await User.findOne({ email })
         const update = { name: req.body.name, email: req.body.email, about: req.body.about, profileImg: req.body.profileImg }
-        User.findByIdAndUpdate({ _id: req.params.id },update,{ new: true, runValidators: true},
+        User.findByIdAndUpdate({ _id: user.id }, update, { new: true, runValidators: true },
             function (err, result) {
                 if (err) {
                     res.send(err)
