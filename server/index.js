0bb39cce8d916fc1244cc8ea784234ef74aec5d3
GeovanaRamos <geovannna.1998@gmail.com>
2020-09-24 15:55:47 -0300

    Adicionando testes com jest e supertest

--- server/index.js
+++ server/index.js
@@ -9,18 +9,23 @@ const app = express();
 app.use(cors());
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
+    useNewUrlParser: true,
 });
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
 db.once('open', function () {
-  console.log("DB connected");
+    console.log("DB connected");
 });
 
 app.use(express.json());
 app.use(userRouter);
 
-app.listen(process.env.PORT != null ? process.env.PORT : 9000, () => {
-  console.log('Server started on port ' + process.env.PORT + '!');
+if (process.env.NODE_ENV == 'test'){}
+    process.env.PORT = 0;
+
+server = app.listen(process.env.PORT != null ? process.env.PORT : 9000, () => {
+    console.log('Server started on port ' + process.env.PORT + '!');
 });
+
+module.exports = server;
