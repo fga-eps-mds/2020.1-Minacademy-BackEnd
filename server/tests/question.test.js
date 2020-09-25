1ba8fc22b65799dd7779d5deaf156487ee4ca366
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-25 20:28:45 -0300

    Testes: Correção nos testes para endpoints de questões

--- server/tests/question.test.js
+++ server/tests/question.test.js
@@ -1,34 +1,51 @@
-const request = require('supertest')
+const mongoose = require('mongoose');
+const supertest = require('supertest')
+
 const app = require('../app')
+const request = supertest(app)
+
 const Module = require('../models/Module')
 
-test('Should get all questions', async (done) => {
-   const response = await request(app).get('/questions')
-      .send()
-      .expect(200);
+describe('Questions', () => {
+   beforeAll(async () => {
+      mongoose.connect(process.env.MONGO_URL, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true,
+         useFindAndModify: false,
+      });
+   });
+
+   afterAll(async (done) => {
+      await mongoose.connection.close();
+      done();
+   });
+
+   it('Should get all questions', async () => {
+      const response = await request.get('/questions')
+         .send()
+         .expect(200);
 
       expect(response.body.length).not.toBeNull();
-      done()
-})
+   })
 
-test('Should get all questions for module 1', async (done) => {
-   const response = await request(app).get('/questions?moduleNumber=1')
-      .send()
-      .expect(200);
+   it('Should get all questions for module 1', async () => {
+      const response = await request.get('/questions?moduleNumber=1')
+         .send()
+         .expect(200);
 
       expect(response.body.length).not.toBeNull();
 
       const module = await Module.findById(response.body[0].module)
       expect(module).not.toBeNull()
       expect(module.moduleNumber).toBe(1)
-      done()
-})
+   })
 
-test('Should not get questions', async (done) => {
-   const response = await request(app).get('/questions?moduleNumber=0')
-      .send()
-      .expect(400);
+   it('Should not get questions', async () => {
+      const response = await request.get('/questions?moduleNumber=0')
+         .send()
+         .expect(400);
 
       expect(response.body.error).toEqual('Modulo não encontrado');
-      done()
-})
+   })
+});
