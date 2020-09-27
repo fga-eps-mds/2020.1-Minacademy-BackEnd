0e62f2c5860c380ebe92868c9f56c2ecdfc64fc8
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-27 18:56:51 -0300

    Question: Adicionado prop para resposta correta
    - Método toJSON para remover a resposta correta das requisições

--- server/models/Question.js
+++ server/models/Question.js
@@ -14,11 +14,24 @@ const questionSchema = new mongoose.Schema({
          type: String,
       }
    },
+   answer: {
+      type: String,
+      required: true,
+   },
    module: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
       ref: 'Module'
    }
 });
+
+questionSchema.methods.toJSON = function () {
+   const question = this
+   const questionObject = question.toObject()
+
+   delete questionObject.answer
+
+   return questionObject
+}
 
 module.exports = mongoose.model('Question', questionSchema);
