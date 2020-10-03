8909e1c57ba8f53a24edb7b5f3a7700fd28cd046
luucas-melo <lucasmelodos322@gmail.com>
2020-10-02 23:47:24 -0300

    Testes: aumento da cobertura de testes
    Co-authored-by: Rocsantos <ro12062000@gmail.com>
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>
    Co-authored-by: peHaick <pedrohaickzz@gmail.com>
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>

--- server/tests/module.test.js
+++ server/tests/module.test.js
@@ -3,7 +3,9 @@ const request = require('supertest')
 const app = require('../app')
 
 const Module = require('../models/Module')
+const User = require('../models/User')
 const { modules } = require('./fixtures/tutorial')
+const { userOne } = require('./fixtures/db')
 
 describe('Modules', () => {
    beforeAll(async () => {
@@ -14,9 +16,11 @@ describe('Modules', () => {
          useFindAndModify: false,
       });
       await Module.insertMany(modules)
+      await new User(userOne).save()
    });
 
    afterAll(async (done) => {
+      await mongoose.connection.dropDatabase()
       await mongoose.connection.close();
       done();
    });
@@ -24,6 +28,7 @@ describe('Modules', () => {
    it('Should return modules list', async () => {
       const response = await request(app).get('/modules')
          .send()
+         .set('Cookie', [`auth_token=${userOne.tokens[0].accessToken}`])
          .expect(200)
 
          expect(response.body.length).not.toBeNull()
