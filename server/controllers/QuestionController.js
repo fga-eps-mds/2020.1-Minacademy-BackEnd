aea027bfbb6b806db0d1164654a779a4ee56e8fe
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-10-08 00:58:05 -0300

    QuestionController: Removido codigo que pertence ao AnswerController
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/67
    
    Co-authored-by: luucas-melo <lucasmelodos322@gmail.com>

--- server/controllers/QuestionController.js
+++ server/controllers/QuestionController.js
@@ -1,6 +1,5 @@
 const Module = require('../models/Module')
 const Question = require('../models/Question')
-const QuestionResult = require('../models/questionResult')
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
@@ -12,60 +11,6 @@ module.exports = {
          if (!module) throw new Error('Modulo não encontrado')
          await module.populate('questions').execPopulate()
          res.send(module.questions)
-      } catch (error) {
-         console.log(error)
-         res.status(400).send({ error: error.message })
-      }
-   },
-
-   async answerQuestion(req, res) {
-      const question = await Question.findById(req.body.question)
-
-      try {
-         if (!question) throw new Error('Questão nao encontrada')
-         const isCorrect = question.answer === req.body.alternative
-
-         const previousQuestionResult = await req.user.execPopulate({
-            path: 'questionResults',
-            match: {
-               question: mongoose.Types.ObjectId(req.body.question)
-            }
-         }).then(user => user.questionResults.length === 1 ? user.questionResults[0] : null)
-
-         if (previousQuestionResult) {
-            previousQuestionResult.alternative = req.body.alternative
-            previousQuestionResult.isCorrect = isCorrect
-            await previousQuestionResult.save()
-            res.send(previousQuestionResult)
-         } else {
-            const questionResult = await new QuestionResult({
-               ...req.body,
-               isCorrect,
-               user: req.user._id,
-            })
-            await questionResult.save()
-            res.status(201).send(questionResult)
-         }
-      } catch (error) {
-         console.log(error)
-         res.status(400).send({ error: error.message })
-      }
-   },
-
-   async getQuestionsResults(req, res) {
-      const match = {}
-      if (req.query.completed)
-         match.isCorrect = req.query.completed === 'true'
-
-      if (req.query.questions)
-         match.question = { $in: req.query.questions }
-
-      try {
-         await req.user.execPopulate({
-            path: 'questionResults',
-            match
-         })
-         res.send(req.user.questionResults)
       } catch (error) {
          console.log(error)
          res.status(400).send({ error: error.message })
