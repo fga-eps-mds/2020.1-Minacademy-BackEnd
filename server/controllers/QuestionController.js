98492d34d8e29a241c2486f1bcb40113c78c2a55
luucas-melo <lucasmelodos322@gmail.com>
2020-10-09 19:22:35 -0300

    QuestionController: adicionado query para filtrar questoes por modulo
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/67
    
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- server/controllers/QuestionController.js
+++ server/controllers/QuestionController.js
@@ -7,13 +7,18 @@ module.exports = {
    async getQuestions(req, res) {
       const query = req.query
       try {
-         const module = await Module.findOne(query)
-         if (!module) throw new Error('Modulo não encontrado')
-         await module.populate('questions').execPopulate()
-         res.send(module.questions)
+         if (!query.moduleNumber){
+             const questions = await Question.find({})
+             res.send(questions)
+         } else {
+            const module = await Module.findOne(query)
+            if (!module) throw new Error('Modulo não encontrado')
+            await module.populate('questions').execPopulate()
+            res.send(module.questions)
+         }
       } catch (error) {
          console.log(error)
          res.status(400).send({ error: error.message })
       }
-   }
+   },
 }
