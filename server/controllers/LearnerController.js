b452dd06a71aeff58793b2f4822dfc93e78c2187
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-10-20 21:57:03 -0300

    LearnerController: Modificado status do aprendiz ao solicitar um mentor
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/78

--- server/controllers/LearnerController.js
+++ server/controllers/LearnerController.js
@@ -34,13 +34,14 @@ module.exports = {
       mentor = await Mentor.findById(mentor._id);
 
       learner.mentor = mentor._id;
+      learner.mentor_request = false
       mentor.learners = mentor.learners.concat(learner._id);
       mentor.isAvailable = false;
 
       await learner.save();
       await mentor.save();
 
-      return res.status(200).send(mentor);
+      return res.status(200).send({ mentorRequest: learner.mentor_request, mentor});
     } catch (err) {
       console.log(err); // eslint-disable-line no-console
       return res.status(400).send({ error: err.message, mentor_request: learner.mentor_request });
@@ -66,10 +67,10 @@ module.exports = {
       await Mentor.findByIdAndUpdate(learner.mentor, { $pull: { learners: learner._id } });
       learner.mentor = null;
       await learner.save();
-      res.send({ mentor: learner.mentor });
+      res.send({ mentorRequest: learner.mentor_request, mentor: learner.mentor });
     } catch (error) {
       console.log(error); // eslint-disable-line no-console
-      res.status(400).send({ error: error.message });
+      res.status(400).send({ mentor: learner.mentor, error: error.message });
     }
   },
 };
