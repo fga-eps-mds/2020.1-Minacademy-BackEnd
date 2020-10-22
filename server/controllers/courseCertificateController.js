c817a0e37d82568cf8c11a667e341b855f1a7d39
luucas-melo <lucasmelodos322@gmail.com>
2020-10-22 00:46:34 -0300

    Certificate: Implementado controller para certificados
    - Criado model de certificado
    - criado rota para certificado
    
    Co-authored-by: peHaick <pedrohaickzz@gmail.com>
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>

---
+++ server/controllers/courseCertificateController.js
@@ -0,0 +1,51 @@
+const jwt = require('jsonwebtoken');
+const CourseCertificate = require('../models/CourseCertificate');
+const userAuth = require('../config/userAuth');
+const User = require('../models/User');
+
+module.exports = {
+  async generateCertificate(req, res) {
+    const { user } = req;
+
+    try {
+      const certificateData = {
+        user: user._id,
+        courseType: user.userType,
+        key: jwt.sign({ _id: user._id }, userAuth.secret),
+      };
+
+      const hasLearnerCertificate = await CourseCertificate.find({
+        user: user._id,
+        courseType: 'Learner',
+      });
+
+      if (hasLearnerCertificate.length === 0) {
+        const certificate = await CourseCertificate.create(certificateData);
+        if (user.userType === 'Learner') {
+          user.courseCertificates.push(certificate._id);
+        }
+        await certificate.save();
+        await user.save();
+
+        res.status(200).send({ certificate });
+      } else {
+        throw new Error('you already have a learner certificate');
+      }
+    } catch (error) {
+      return res.status(400).send({ error: error.message });
+    }
+  },
+
+  async getLearnerCertificate(req, res) {
+    const { _id } = req.body;
+
+    try {
+      const certificate = await CourseCertificate.findById(_id);
+      const user = await User.findById(certificate.user);
+      certificate.user = user;
+      return res.status(200).send({ certificate });
+    } catch (error) {
+      return res.status(400).send({ error: error.message });
+    }
+  },
+};
