68edecca9e931b8e2d6fbedef2f994b81c4425d3
Rodrigo <ro12062000@gmail.com>
2020-10-28 17:29:35 -0300

    Adicionado certificado de mentoria

--- server/controllers/courseCertificateController.js
+++ server/controllers/courseCertificateController.js
@@ -11,20 +11,36 @@ module.exports = {
       user: user._id,
       courseType: 'Learner',
     });
+
     try {
-      const certificateData = {
-        user: user._id,
-        courseType: user.userType,
-        key: jwt.sign({ _id: user._id }, userAuth.secret),
-      };
-      // console.log(hasLearnerCertificate);
-      if (hasLearnerCertificate.length === 0) {
+      if (hasLearnerCertificate.length === 0 && user.userType === 'Learner') {
+        const certificateData = {
+          user: user._id,
+          courseType: user.userType,
+          key: jwt.sign({ _id: user._id }, userAuth.secret),
+        };
+
         const certificate = await CourseCertificate.create(certificateData);
-        if (user.userType === 'Learner') {
-          user.courseCertificates.push(certificate._id);
-        }
+        user.courseCertificates.push(certificate._id);
         await certificate.save();
         await user.save();
+
+        const mentor = user
+          .execPopulate('mentor')
+          .then((user) => user.mentor);
+        
+        if (mentor){
+          const mentorCertificateData = {
+            user: mentor._id,
+            courseType: 'Mentor ' + user._id,
+            key: jwt.sign({ _id: mentor._id + user._id }, userAuth)
+          };
+
+          const mentorCertificate = await CourseCertificate.create(mentorCertificateData);
+          mentor.courseCertificates.push( mentorCertificate._id );
+          await mentorCertificate.save();
+          await mentor.save();
+        }
 
         res.status(200).send(certificate);
       } else {
@@ -42,7 +58,6 @@ module.exports = {
 
   async getLearnerCertificate(req, res) {
     const { _id } = req.body;
-
     try {
       const certificate = await CourseCertificate.findById(_id);
       const user = await User.findById(certificate.user);
