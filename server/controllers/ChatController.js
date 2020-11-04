684084527ce316bb1006653a0258e57ef9ac9da2
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-11-03 21:29:46 -0300

    ChatController: Implementado controller para chats
    Co-authored-by: luucas-melo <lucasmelodos322@gmail.com>

--- server/controllers/ChatController.js
+++ server/controllers/ChatController.js
@@ -1,17 +1,61 @@
+const Chat = require('../models/Chat');
+const { Message } = require('../models/Message');
 const { connections } = require('../websocket')
+const { findSockets } = require('../utils/findSocket')
 
 module.exports = {
-  async teste(req, res) {
-    req.io.emit('TESTE', 'ola')
-    for (const key in req.io.sockets) {
-      req.io.sockets[key].emit('TESTE', key)
+  async getChat(req, res) {
+    const { user } = req
+
+    try {
+     const chat = await user.execPopulate('chat').then(doc => doc.chat)
+     console.log(chat)
+     res.send(chat)
+    } catch (error) {
+      console.error(error)
     }
-    const targets = connections['5f8f15aafdc23200162c7ee6']
-    if (targets) {
-      targets.forEach((target) => target.emit("TESTE", "SOMENTE VOCE RECEBE 0!"))
-    }
-    const receiver = Object.values(req.io.sockets).find((item) => item.decoded_token.id == '5f8f15aafdc23200162c7ee6')
-    // receiver.emit('TESTE', "somente voce recebe!")
-    res.send({ clients: Object.keys(connections) })
   },
+
+  async createChat(ids) {
+    try {
+      let chat = await Chat.findOne({ users: ids })
+      if (chat) return
+      chat = new Chat({ users: ids })
+      await chat.save()
+      const targets = findSockets(ids)
+      targets.forEach((target) => target.emit('assigned', chat))
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async sendMessage(req, res) {
+    const { toChat, content } = req.body
+    try {
+      const chat = await Chat.findById(toChat)
+      const newMessage = await new Message({ sender: req.user._id, content });
+
+      chat.messages = chat.messages.concat(newMessage)
+
+      await newMessage.save();
+      await chat.save();
+
+      // const targets = findSockets([...chat.users])
+      // targets.forEach((target) => target.emit('assigned', newMessage))
+      req.io.to(chat._id).emit("assigned", newMessage)
+
+      res.send(chat)
+    } catch (error) {
+      console.error(error);
+      res.status(400).send({ error: error.message });
+    }
+  },
+  async teste(req, res) {
+    // console.log(connections[req.user._id][0].adapter.rooms)
+    Object.keys(connections).forEach((item) => {
+      connections[item].forEach((x) => x.emit('assigned', 'teste'))
+    })
+    req.io.emit('assigned', "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    res.send('OK')
+  }
 };
--- server/routers/chat.js
+++ server/routers/chat.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 const ChatController = require('../controllers/ChatController');
+const auth = require('../middleware/authentication');
 // const UserController = require('../controllers/UserController');
 // const auth = require('../middleware/authentication');
 
 const router = new Router();
 
-router.get('/chat', ChatController.teste)
+router.get('/teste', auth, ChatController.teste)
+router.get('/chat', auth, ChatController.getChat)
+router.post('/chat', auth, ChatController.sendMessage)
 
 module.exports = router;
 
