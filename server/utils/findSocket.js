fb56aa048cf8352a9dca6fa0bbca2c92865ce337
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-11-03 21:27:58 -0300

    Util: Criado função para buscar sockets por id
    Co-authored-by: luucas-melo <lucasmelodos322@gmail.com>

---
+++ server/utils/findSocket.js
@@ -0,0 +1,12 @@
+const { connections } = require("../websocket")
+
+module.exports = {
+  findSockets(ids) {
+    const sockets = []
+    ids.forEach((id) => {
+      if (connections[id]) sockets.push(...connections[id])
+    })
+
+    return sockets
+  }
+}
