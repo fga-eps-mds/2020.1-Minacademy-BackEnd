5ff6d20dbde1a826245d80e1d1c7a9ae010ee7d6
luucas-melo <lucasmelodos322@gmail.com>
2020-11-04 22:09:14 -0300

    Websocket: adicionado try catch
    Co-authored-by: EduardoNunes <eduardonpicolo@gmail.com>

--- server/websocket.js
+++ server/websocket.js
@@ -4,6 +4,7 @@ const User = require('./models/User');
 
 const connections = {};
 
+socketio.origins('*:*')
 socketio.of('/api').use(socketioJwt.authorize({
   secret: 'efb7e5eddfdd6ac729b741a2946a89a58dee7569bd4962d4bc', // TIRA DO REPOSITORIO!!!
   handshake: true
@@ -15,17 +16,21 @@ socketio.of('/api').on('connection', async (socket) => {
   console.log("user ID: ", socket.decoded_token.id);
   console.log("------------------------");
 
-  if (connections[socket.decoded_token.id]) {
-    console.log("JA EXISTE")
-    connections[socket.decoded_token.id].push(socket);
-  } else {
-    connections[socket.decoded_token.id] = [socket];
-  }
+  try {
+    if (connections[socket.decoded_token.id]) {
+      console.log("JA EXISTE")
+      connections[socket.decoded_token.id].push(socket);
+    } else {
+      connections[socket.decoded_token.id] = [socket];
+    }
 
-  const user = await User.findById(socket.decoded_token.id)
-  const chats = await user.execPopulate('chat').then(doc => doc.chat)
-  console.log('CHATS: ', chats)
-  chats.forEach((chat) => socket.join(chat._id))
+    const user = await User.findById(socket.decoded_token.id)
+    const chats = await user.execPopulate('chat').then(doc => doc.chat)
+    console.log('CHATS: ', chats)
+    chats.forEach((chat) => socket.join(chat._id))
+  } catch (error) {
+    console.error(error)
+  }
 
   socket.on('disconnect', (reason) => {
     console.log('disconnect', reason);
