eab405275968fcff273dd8410c2b1ba64796fe35
estevaoreis <estevaoreis1587@gmail.com>
2020-11-13 12:38:38 -0300

    Corrigindo o lint

--- server/mail/index.js
+++ server/mail/index.js
@@ -10,21 +10,19 @@ if (process.env.NODE_ENV === 'production') {
       pass: process.env.MAIL_PASSWORD,
     },
   });
-} else if(process.env.NODE_ENV === 'test'){
-  const nodemailerMock = require('nodemailer-mock');
+} else if (process.env.NODE_ENV === 'test') {
+  const nodemailerMock = require('nodemailer-mock'); // eslint-disable-line global-require
   transport = nodemailerMock.createTransport({
     host: 'qualquerhost',
     port: 'qualquerporta',
     auth: null,
   });
-} 
-else {
+} else {
   transport = nodemailer.createTransport({
     host: process.env.MAILHOG_HOST,
     port: '1025',
     auth: null,
   });
-  console.log("Entrou no ELSE DO MAILLLHOOOG");
 }
 
 module.exports = transport;
