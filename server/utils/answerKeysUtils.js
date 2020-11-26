b10fde702a7f3fc9ee8100b587eab3cb79c7558c
peHaick <pedrohaickzz@gmail.com>
2020-11-25 22:51:18 -0300

    AnswerKeysUtils: métricas do SonarCloud

--- server/utils/answerKeysUtils.js
+++ server/utils/answerKeysUtils.js
@@ -7,9 +7,9 @@ module.exports = {
   /* eslint no-shadow: ["error", { "allow": ["user"] }] */
   /* eslint-env es6 */
   async populateAnswerKeys(user) {
-    const answerKeys = await user.execPopulate('answers').then((user) => {
-      if (!user.answers) throw new Error('User does not have any answered question');
-      return user.answers.populate({
+    const answerKeys = await user.execPopulate('answers').then((userData) => {
+      if (!userData.answers) throw new Error('User does not have any answered question');
+      return userData.answers.populate({
         path: 'answers',
         populate: {
           path: 'question',
